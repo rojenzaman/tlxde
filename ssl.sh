142e615f0bbbb7de52baa5a7db04d4bb3c857c74
Rojen Zaman <rojen@riseup.net>
2022-03-11 14:30:35 +0300

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p ssl
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ssl/nginx.key -out ssl/nginx.crt
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ssl/nginx.key -out ssl/nginx.crt -batch
